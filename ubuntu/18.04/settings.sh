9716aa81f8d9a7d1cf9d39af34014c6277bbe6f1
Joe <success1@gmail.com>
2019-01-22 22:00:31 -0500

    Fixed docker repository add

--- ubuntu/18.04/settings.sh
+++ ubuntu/18.04/settings.sh
@@ -2,7 +2,8 @@
 echo "# set infinite history size" >> ~/.bashrc
 echo "HISTSIZE=" >> ~/.bashrc
 echo "HISTFILESIZE=" >> ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
+
 
 
    
--- ubuntu/18.04/software.sh
+++ ubuntu/18.04/software.sh
@@ -9,7 +9,10 @@ rm -f "$TEMP_FILE"
 # -- dependencies
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
 # docker-machine
