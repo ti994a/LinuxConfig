71389ab81c1ea73e4259978df299fee966bde994
Joe <success1@gmail.com>
2019-02-02 09:14:19 -0500

    added setttings for Ubuntu 16.04

--- ubuntu/16.04/settings.sh
+++ ubuntu/16.04/settings.sh
@@ -3,9 +3,12 @@ echo "# set infinite history size" >> ~/.bashrc
 echo "HISTSIZE=" >> ~/.bashrc
 echo "HISTFILESIZE=" >> ~/.bashrc
 . ~/.bashrc
+# move unity desktop launcher bar to bottom of screen
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+# set default Files view to list
+gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 # install conky-hentry theme
 cd conky-henry/
 sh install.sh
 
 
-   
